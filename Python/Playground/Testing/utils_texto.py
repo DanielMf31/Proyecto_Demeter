6e35a0948ab39c8bc96a4e0894e949ce6c751c61
DanielMf31 <dmf310506@gmail.com>
2026-02-18 08:29:38 +0100

    Hotfix: Update WebSocket connection to WSS and improve Nginx headers

--- Python/Playground/Testing/utils_texto.py
+++
@@ -1,6 +0,0 @@
-def es_palindromo(texto):
-    """Devuelve True si el texto se lee igual al revés."""
-    if not isinstance(texto, str):
-        return False
-    texto_limpio = texto.replace(" ", "").lower()
-    return texto_limpio == texto_limpio[::-1]
